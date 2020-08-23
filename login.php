8024966cac36a499beb0beb65542b23e753c57c4
rackoonstore <70112482+rackoonstore@users.noreply.github.com>
2020-08-23 15:14:23 -0500

    Add files via upload

---
+++ login.php
@@ -0,0 +1,27 @@
+<?php
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "test";
+
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+if (!$conn)
+{
+  die("No hay conexión: ".mysqli_connect_error());
+}
+$nombre = $_POST["txtusuario"];
+$pass = $_POST["txtpassword"];
+
+$query = mysqli_query($conn, "SELECT * FROM login WHERE usuario = '".$nombre."' and password ='".$pass."'");
+$nr = mysqli_num_rows($query);
+
+if ($nr == 1)
+ {
+  header("Location: 1Proyecto/1index.php");
+}
+else if ($nr == 0)
+{
+  header("Location: login.html");
+  //echo "No ingreso";
+}
+ ?>
